8a81f82afffb0268d1bcf46e5dc425bba91f9d27
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2021-10-01 23:29:28 +0300

    Add Multi Math Full Game

---
+++ app/scoreboard.ts
@@ -0,0 +1,29 @@
+/// <reference path="result.ts" />
+
+class Scoreboard {
+  private results: Result[] = [];
+
+  addResult(newResult: Result): void {
+    this.results.push(newResult);
+  }
+  updateScoreBoard(): void {
+    let output: string = "<h2>Scoreboard</h2>";
+
+    for (let index = 0; index < this.results.length; index++) {
+      const result: Result = this.results[index];
+      output += "<h4>";
+      output +=
+        result.playerName +
+        ": " +
+        result.score +
+        "/" +
+        result.problemCount +
+        ": " +
+        "for factor " +
+        result.factor;
+      output += "</h4>";
+    }
+    const scoresElement: HTMLElement | null = document.getElementById("scores");
+    scoresElement!.innerHTML = output;
+  }
+}
